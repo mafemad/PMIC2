7c08b77464cf522cfff78c5a4710194feaed605d
mafemad <mafemad2005@gmail.com>
2023-04-14 18:33:04 -0300

    att

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask("__name__")
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", background= url_for('static', filename='imagens/fundo.png'))
 
 @app.route("/quemsomos")
 def quemsomos():
-    return render_template("quemsomos.html")
+    return render_template("quemsomos.html", background= url_for('static', filename='imagens/fundo2.png'))
 
 @app.route("/contato")
 def contato():
-    return render_template("contato.html")
+    return render_template('contato.html', background= url_for('static', filename='imagens/fundo2.png'))
+
+app.run(debug=True)
